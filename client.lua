af6f207399352f798b5f84ba9e632b5589dc1515
SteakHarpyie59 <77833916+SteakHarpyie59@users.noreply.github.com>
2023-03-12 12:22:18 +0100

    Removing esx:getSharedObject
    Since esx:getSharedObject is no longer supportet by newer Versions, i rewrote the script like in https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent/

--- client.lua
+++ client.lua
@@ -1,17 +1,6 @@
-ESX                 = nil
 PlayerData			= {}
 
-
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-
-	while ESX.GetPlayerData().job == nil do
-		Citizen.Wait(10)
-	end
-
 	PlayerData = ESX.GetPlayerData()
 end)
 
